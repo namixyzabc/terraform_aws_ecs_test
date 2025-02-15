b74fe77c057c9f5d114d79b2bd4c4208358d5862
namixyzabc <namixyzabc@gmail.com>
2025-02-15 16:07:29 +0900

    s3_bucketID_重複していたため削除

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -12,8 +12,3 @@ output "bucket_regional_domain_name" {
   description = "The ID of the S3 bucket"
   value       = aws_s3_bucket.bucket.id
 }
-
-output "bucket_id" {
-  description = "The ID of the S3 bucket"
-  value       = aws_s3_bucket.bucket.id
-}
