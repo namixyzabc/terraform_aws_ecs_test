d8637b56e0ebecd0c7a8861c6a63152607ab1a16
namixyzabc <namixyzabc@gmail.com>
2025-02-15 17:10:53 +0900

    acm_main_resource "aws_acm_certificate_validation" "validation" __provider                = aws.us_east_1を追加

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -31,6 +31,7 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "validation" {
+  provider                = aws.us_east_1 
   certificate_arn         = aws_acm_certificate.certificate.arn
   validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
 }
