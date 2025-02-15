8603375ec814950926615e07b45f1dac34a46965
namixyzabc <namixyzabc@gmail.com>
2025-02-15 16:01:01 +0900

    rds/main.tf の aws_db_instance リソースからname = var.db_nameを削除（AWS Provider 5.x 以降では、aws_db_instance リソースで name 引数はサポートされない）

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -4,7 +4,6 @@ resource "aws_db_instance" "default" {
   engine                 = "mysql"
   engine_version         = "8.0"
   instance_class         = "db.t3.micro"
-  name                   = var.db_name
   username               = var.db_username
   password               = var.db_password
   parameter_group_name   = "default.mysql8.0"
