c471b7e588af2abf13fc112bea4203e14631a3cb
namixyzabc <namixyzabc@gmail.com>
2025-02-15 15:45:37 +0900

    S3＿output_bucket_idを追加

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -7,3 +7,8 @@ output "bucket_regional_domain_name" {
   description = "The regional domain name of the S3 bucket"
   value       = aws_s3_bucket.bucket.bucket_regional_domain_name
 }
+
+ output "bucket_id" {
+  description = "The ID of the S3 bucket"
+  value       = aws_s3_bucket.bucket.id
+}
