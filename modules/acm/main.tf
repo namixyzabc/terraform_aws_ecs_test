f7d76455ab1dfe634f7eb26a2d82327edadba7a0
namixyzabc <namixyzabc@gmail.com>
2025-02-15 16:40:04 +0900

    acm_main_ACM 証明書を us-east-1 で発行する内容へ修正

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,6 +1,12 @@
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 resource "aws_acm_certificate" "certificate" {
-  domain_name               = var.domain_name
-  validation_method         = "DNS"
+  provider            = aws.us_east_1
+  domain_name         = var.domain_name
+  validation_method   = "DNS"
 
   lifecycle {
     create_before_destroy = true
